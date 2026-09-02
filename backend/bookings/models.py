a7de69b8059bf5b385170e28f991181ae794bc30
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2026-09-02 10:05:03 -0300

    desenvolvimento da app de reservas

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+from listings.models import Imovel
+from django.conf import settings
 
+class Reserva(models.Model):
+    imovel_reservado = models.ForeignKey(Imovel,on_delete=models.PROTECT ,related_name='imovel_reservado')
+    usuario_inquilino = models.ForeignKey(settings.AUTH_USER_MODEL ,on_delete= models.PROTECT, related_name='inquilino')
+    check_in = models.DateField()
+    check_out = models.DateField()
+    #status_reserva = models.CharField(max_length=20)
+    data_criacao_reserva = models.DateTimeField(auto_now_add=True)
